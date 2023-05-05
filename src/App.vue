9c187d63fd446e0d62b1dade507446d9cf6ec7f1
Edoardo-Monti <122456302+Edoardo-Monti@users.noreply.github.com>
2023-05-05 12:57:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+
+
+export default{
+  name: 'App',
+  components: {
+    
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-center">ciao</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./style/main.scss"
+</style>
