6f6ecfd46d61466096f3c7bc90f83e62c4cfd21b
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-05 15:19:40 +0200

    creazione componeneti header e main

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <script >
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+
 
 
 export default{
   name: 'App',
   components: {
-    
-  }
+    HeaderComp,
+    MainComp
+}
 }
 
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center">ciao</h1>
+  <HeaderComp/>
+  <div class="background-orange">
+    <div class="second-bg">
+      <MainComp/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-@use "./style/main.scss"
+@use "./style/main.scss";
+
+.background-orange{
+  background-color: orange;
+  height: 100vh;
+  display: flex;
+  .second-bg{
+    width: 80%;
+    margin: 0 auto;
+    height: 500px;
+    margin-top: 50px;
+    background-color: white;
+  }
+}
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name:"Header"
+}
+</script>
+<template>
+    <div>
+        <h1 class="p-3">Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
